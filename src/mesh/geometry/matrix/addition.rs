afc27e4ce95362b5effdd612683b74e8218ec7e6
Ryan Sullivan <ryansully96@googlemail.com>
2022-06-26 12:57:00 +0000

    Made matrix addition and subtraction more generic

--- src/mesh/geometry/matrix/addition.rs
+++ src/mesh/geometry/matrix/addition.rs
@@ -5,35 +5,52 @@ use std::ops::{Add, AddAssign};
 
 use super::Matrix;
 
-macro_rules! add_impl {
-    ({$lhs_t:ty} + {$rhs_t:ty}) => {
-        impl<const R: usize, const C: usize> Add<$rhs_t> for $lhs_t {
-            type Output = Matrix<R, C>;
+impl<T, const R: usize, const C: usize> Add<T> for Matrix<R, C>
+where
+    T: AsRef<Matrix<R, C>>,
+{
+    type Output = Matrix<R, C>;
 
-            fn add(self, rhs: $rhs_t) -> Self::Output {
-                let mut mat = self.clone();
-                mat.iter_mut()
-                    .zip(rhs.iter())
-                    .for_each(|(lhs, rhs)| lhs.add_assign(rhs));
-                mat
-            }
-        }
-    };
-    ({$lhs_t:ty} += {$rhs_t:ty}) => {
-        impl<const R: usize, const C: usize> AddAssign<$rhs_t> for $lhs_t {
-            fn add_assign(&mut self, rhs: $rhs_t) {
-                self.iter_mut()
-                .zip(rhs.iter())
-                .for_each(|(lhs, rhs)| lhs.add_assign(rhs));
-            }
-        }
-    };
+    fn add(self, rhs: T) -> Self::Output {
+        let mut mat = self;
+        mat.iter_mut()
+            .zip(rhs.as_ref().iter())
+            .for_each(|(lhs, rhs)| lhs.add_assign(rhs));
+        mat
+    }
+}
+impl<T, const R: usize, const C: usize> Add<T> for &Matrix<R, C>
+where
+    T: AsRef<Matrix<R, C>>,
+{
+    type Output = Matrix<R, C>;
+
+    fn add(self, rhs: T) -> Self::Output {
+        let mut mat = *self;
+        mat.iter_mut()
+            .zip(rhs.as_ref().iter())
+            .for_each(|(lhs, rhs)| lhs.add_assign(rhs));
+        mat
+    }
 }
 
-add_impl! {{Matrix<R, C>} + {Matrix<R, C>}}
-add_impl! {{Matrix<R, C>} + {&Matrix<R, C>}}
-add_impl! {{&Matrix<R, C>} + {Matrix<R, C>}}
-add_impl! {{&Matrix<R, C>} + {&Matrix<R, C>}}
-
-add_impl! {{Matrix<R, C>} += {Matrix<R, C>}}
-add_impl! {{Matrix<R, C>} += {&Matrix<R, C>}}
+impl<T, const R: usize, const C: usize> AddAssign<T> for Matrix<R, C>
+where
+    T: AsRef<Matrix<R, C>>,
+{
+    fn add_assign(&mut self, rhs: T) {
+        self.iter_mut()
+            .zip(rhs.as_ref().iter())
+            .for_each(|(lhs, rhs)| lhs.add_assign(rhs));
+    }
+}
+impl<T, const R: usize, const C: usize> AddAssign<T> for &mut Matrix<R, C>
+where
+    T: AsRef<Matrix<R, C>>,
+{
+    fn add_assign(&mut self, rhs: T) {
+        self.iter_mut()
+            .zip(rhs.as_ref().iter())
+            .for_each(|(lhs, rhs)| lhs.add_assign(rhs));
+    }
+}
--- src/mesh/geometry/matrix/subtraction.rs
+++ src/mesh/geometry/matrix/subtraction.rs
@@ -5,35 +5,52 @@ use std::ops::{Sub, SubAssign};
 
 use super::Matrix;
 
-macro_rules! add_impl {
-    ({$lhs_t:ty} - {$rhs_t:ty}) => {
-        impl<const R: usize, const C: usize> Sub<$rhs_t> for $lhs_t {
-            type Output = Matrix<R, C>;
+impl<T, const R: usize, const C: usize> Sub<T> for Matrix<R, C>
+where
+    T: AsRef<Matrix<R, C>>,
+{
+    type Output = Matrix<R, C>;
 
-            fn sub(self, rhs: $rhs_t) -> Self::Output {
-                let mut mat = self.clone();
-                mat.iter_mut()
-                    .zip(rhs.iter())
-                    .for_each(|(lhs, rhs)| lhs.sub_assign(rhs));
-                mat
-            }
-        }
-    };
-    ({$lhs_t:ty} -= {$rhs_t:ty}) => {
-        impl<const R: usize, const C: usize> SubAssign<$rhs_t> for $lhs_t {
-            fn sub_assign(&mut self, rhs: $rhs_t) {
-                self.iter_mut()
-                .zip(rhs.iter())
-                .for_each(|(lhs, rhs)| lhs.sub_assign(rhs));
-            }
-        }
-    };
+    fn sub(self, rhs: T) -> Self::Output {
+        let mut mat = self;
+        mat.iter_mut()
+            .zip(rhs.as_ref().iter())
+            .for_each(|(lhs, rhs)| lhs.sub_assign(rhs));
+        mat
+    }
+}
+impl<T, const R: usize, const C: usize> Sub<T> for &Matrix<R, C>
+where
+    T: AsRef<Matrix<R, C>>,
+{
+    type Output = Matrix<R, C>;
+
+    fn sub(self, rhs: T) -> Self::Output {
+        let mut mat = *self;
+        mat.iter_mut()
+            .zip(rhs.as_ref().iter())
+            .for_each(|(lhs, rhs)| lhs.sub_assign(rhs));
+        mat
+    }
 }
 
-add_impl! {{Matrix<R, C>} - {Matrix<R, C>}}
-add_impl! {{Matrix<R, C>} - {&Matrix<R, C>}}
-add_impl! {{&Matrix<R, C>} - {Matrix<R, C>}}
-add_impl! {{&Matrix<R, C>} - {&Matrix<R, C>}}
-
-add_impl! {{Matrix<R, C>} -= {Matrix<R, C>}}
-add_impl! {{Matrix<R, C>} -= {&Matrix<R, C>}}
+impl<T, const R: usize, const C: usize> SubAssign<T> for Matrix<R, C>
+where
+    T: AsRef<Matrix<R, C>>,
+{
+    fn sub_assign(&mut self, rhs: T) {
+        self.iter_mut()
+            .zip(rhs.as_ref().iter())
+            .for_each(|(lhs, rhs)| lhs.sub_assign(rhs));
+    }
+}
+impl<T, const R: usize, const C: usize> SubAssign<T> for &mut Matrix<R, C>
+where
+    T: AsRef<Matrix<R, C>>,
+{
+    fn sub_assign(&mut self, rhs: T) {
+        self.iter_mut()
+            .zip(rhs.as_ref().iter())
+            .for_each(|(lhs, rhs)| lhs.sub_assign(rhs));
+    }
+}
