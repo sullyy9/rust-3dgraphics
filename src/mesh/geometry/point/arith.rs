beb50402eb87e695fa1fcfa91300501eefb4779d
Ryan Sullivan <ryansully96@googlemail.com>
2022-06-26 12:37:26 +0000

    Make point operator overloads more generic.

--- src/mesh/geometry/point/arith.rs
+++ src/mesh/geometry/point/arith.rs
@@ -1,5 +1,5 @@
 //! Implementations of arithmetic operations on points.
-//! 
+//!
 
 use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Sub};
 
@@ -8,93 +8,83 @@ use super::{Matrix, Point, Vector};
 ////////////////////////////////////////////////////////////////////////////////
 // Point + Vector = Point //////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-impl<const D: usize> Add<Vector<D>> for Point<D> {
-    type Output = Point<D>;
-    fn add(self, rhs: Vector<D>) -> Self::Output {
-        Point(self.0.add(&rhs.0))
+impl<T, const N: usize> Add<T> for Point<N>
+where
+    T: AsRef<Vector<N>>,
+{
+    type Output = Point<N>;
+    fn add(self, rhs: T) -> Self::Output {
+        Point(self.0.add(&rhs.as_ref().0))
     }
 }
-impl<const D: usize> Add<&Vector<D>> for Point<D> {
-    type Output = Point<D>;
-    fn add(self, rhs: &Vector<D>) -> Self::Output {
-        Point(self.0.add(&rhs.0))
-    }
-}
-impl<const D: usize> Add<Vector<D>> for &Point<D> {
-    type Output = Point<D>;
-    fn add(self, rhs: Vector<D>) -> Self::Output {
-        Point(self.0.add(&rhs.0))
-    }
-}
-impl<const D: usize> Add<&Vector<D>> for &Point<D> {
-    type Output = Point<D>;
-    fn add(self, rhs: &Vector<D>) -> Self::Output {
-        Point(self.0.add(&rhs.0))
+impl<T, const N: usize> Add<T> for &Point<N>
+where
+    T: AsRef<Vector<N>>,
+{
+    type Output = Point<N>;
+    fn add(self, rhs: T) -> Self::Output {
+        Point(self.0.add(&rhs.as_ref().0))
     }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // Point += Vector /////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-impl<const D: usize> AddAssign<Vector<D>> for Point<D> {
-    fn add_assign(&mut self, rhs: Vector<D>) {
-        self.0.add_assign(&rhs.0);
+impl<T, const N: usize> AddAssign<T> for Point<N>
+where
+    T: AsRef<Vector<N>>,
+{
+    fn add_assign(&mut self, rhs: T) {
+        self.0.add_assign(&rhs.as_ref().0);
     }
 }
-impl<const D: usize> AddAssign<&Vector<D>> for Point<D> {
-    fn add_assign(&mut self, rhs: &Vector<D>) {
-        self.0.add_assign(&rhs.0);
-    }
-}
-impl<const D: usize> AddAssign<Vector<D>> for &mut Point<D> {
-    fn add_assign(&mut self, rhs: Vector<D>) {
-        self.0.add_assign(&rhs.0);
-    }
-}
-impl<const D: usize> AddAssign<&Vector<D>> for &mut Point<D> {
-    fn add_assign(&mut self, rhs: &Vector<D>) {
-        self.0.add_assign(&rhs.0);
+impl<T, const N: usize> AddAssign<T> for &mut Point<N>
+where
+    T: AsRef<Vector<N>>,
+{
+    fn add_assign(&mut self, rhs: T) {
+        self.0.add_assign(&rhs.as_ref().0);
     }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // Point - Point = Vector //////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-impl<const D: usize> Sub<Point<D>> for Point<D> {
-    type Output = Vector<D>;
-    fn sub(self, rhs: Point<D>) -> Self::Output {
-        Vector(self.0.sub(&rhs.0))
+impl<T, const N: usize> Sub<T> for Point<N>
+where
+    T: AsRef<Point<N>>,
+{
+    type Output = Vector<N>;
+    fn sub(self, rhs: T) -> Self::Output {
+        Vector(self.0.sub(&rhs.as_ref().0))
     }
 }
-impl<const D: usize> Sub<&Point<D>> for Point<D> {
-    type Output = Vector<D>;
-    fn sub(self, rhs: &Point<D>) -> Self::Output {
-        Vector(self.0.sub(&rhs.0))
-    }
-}
-impl<const D: usize> Sub<Point<D>> for &Point<D> {
-    type Output = Vector<D>;
-    fn sub(self, rhs: Point<D>) -> Self::Output {
-        Vector(self.0.sub(&rhs.0))
-    }
-}
-impl<const D: usize> Sub<&Point<D>> for &Point<D> {
-    type Output = Vector<D>;
-    fn sub(self, rhs: &Point<D>) -> Self::Output {
-        Vector(self.0.sub(&rhs.0))
+impl<T, const N: usize> Sub<T> for &Point<N>
+where
+    T: AsRef<Point<N>>,
+{
+    type Output = Vector<N>;
+    fn sub(self, rhs: T) -> Self::Output {
+        Vector(self.0.sub(&rhs.as_ref().0))
     }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // Point * Scaler = Point //////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-impl<T: Into<f64>, const D: usize> Mul<T> for Point<D> {
+impl<T, const D: usize> Mul<T> for Point<D>
+where
+    T: Into<f64>,
+{
     type Output = Point<D>;
     fn mul(self, rhs: T) -> Self::Output {
         Point(self.0.mul(rhs))
     }
 }
-impl<T: Into<f64>, const D: usize> Mul<T> for &Point<D> {
+impl<T, const D: usize> Mul<T> for &Point<D>
+where
+    T: Into<f64>,
+{
     type Output = Point<D>;
     fn mul(self, rhs: T) -> Self::Output {
         Point(self.0.mul(rhs))
@@ -104,12 +94,18 @@ impl<T: Into<f64>, const D: usize> Mul<T> for &Point<D> {
 ////////////////////////////////////////////////////////////////////////////////
 // Point *= Scaler /////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-impl<T: Into<f64>, const D: usize> MulAssign<T> for Point<D> {
+impl<T, const D: usize> MulAssign<T> for Point<D>
+where
+    T: Into<f64>,
+{
     fn mul_assign(&mut self, rhs: T) {
         self.0.mul_assign(rhs);
     }
 }
-impl<T: Into<f64>, const D: usize> MulAssign<T> for &mut Point<D> {
+impl<T, const D: usize> MulAssign<T> for &mut Point<D>
+where
+    T: Into<f64>,
+{
     fn mul_assign(&mut self, rhs: T) {
         self.0.mul_assign(rhs);
     }
@@ -118,13 +114,19 @@ impl<T: Into<f64>, const D: usize> MulAssign<T> for &mut Point<D> {
 ////////////////////////////////////////////////////////////////////////////////
 // Point / Scaler = Point //////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-impl<T: Into<f64>, const D: usize> Div<T> for Point<D> {
+impl<T, const D: usize> Div<T> for Point<D>
+where
+    T: Into<f64>,
+{
     type Output = Point<D>;
     fn div(self, rhs: T) -> Self::Output {
         Point(self.0.div(rhs))
     }
 }
-impl<T: Into<f64>, const D: usize> Div<T> for &Point<D> {
+impl<T, const D: usize> Div<T> for &Point<D>
+where
+    T: Into<f64>,
+{
     type Output = Point<D>;
     fn div(self, rhs: T) -> Self::Output {
         Point(self.0.div(rhs))
@@ -134,12 +136,18 @@ impl<T: Into<f64>, const D: usize> Div<T> for &Point<D> {
 ////////////////////////////////////////////////////////////////////////////////
 // Point /= Scaler /////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-impl<T: Into<f64>, const D: usize> DivAssign<T> for Point<D> {
+impl<T, const D: usize> DivAssign<T> for Point<D>
+where
+    T: Into<f64>,
+{
     fn div_assign(&mut self, rhs: T) {
         self.0.div_assign(rhs);
     }
 }
-impl<T: Into<f64>, const D: usize> DivAssign<T> for &mut Point<D> {
+impl<T, const D: usize> DivAssign<T> for &mut Point<D>
+where
+    T: Into<f64>,
+{
     fn div_assign(&mut self, rhs: T) {
         self.0.div_assign(rhs);
     }
